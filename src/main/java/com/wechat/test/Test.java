6c50aec5efdf0d39a51082f69ded2ea9f5c96bdd
lvlq73 <41459910+lvlq73@users.noreply.github.com>
2018-07-20 12:42:38 +0800

    Create Test.java

---
+++ src/main/java/com/wechat/test/Test.java
@@ -0,0 +1,22 @@
+package com.wechat.test;
+
+import com.wechat.entity.WeChatPayParams;
+import com.wechat.interfaces.IPayAfterSuccess;
+import com.wechat.pay.WeChatPayUtil;
+
+public class Test {
+
+    public static void main(String args[]){
+        //测试统一下单成功后
+//        WeChatPayParams params = new WeChatPayParams();
+//        params.obj = new TestAfterSuccsess();
+//        System.out.println("1");
+//        WeChatPayUtil.wechatPayTest(params);
+//        System.out.println("2");
+        //测试支付成功后
+        IPayAfterSuccess a =  new TestAfterSuccsess();
+        System.out.println("1");
+        WeChatPayUtil.wxPaymentResultTest(null,null,a);
+        System.out.println("2");
+    }
+}
