2dc2fbe838a979c753a68452928b372f399b81f8
lvlq73 <41459910+lvlq73@users.noreply.github.com>
2018-07-20 12:32:47 +0800

    Create Content.java

---
+++ src/main/java/com/wechat/config/Content.java
@@ -0,0 +1,31 @@
+package com.wechat.config;
+
+public class Content {
+    /**
+     * 成功
+     */
+    public  final  static  String  SUCCESS="SUCCESS";
+    public  final  static  String  FAIL="FAIL";
+
+    /**
+     * 统一下单
+     */
+    public final  static  String UNIFIEDORDER="https://api.mch.weixin.qq.com/pay/unifiedorder";
+
+    /**
+     * 查询订单   使用场景（第一次支付 下单成功支付失败的时候 还要进行支付要 调用查询订单）
+     */
+    public final  static  String ORDERQUERY="https://api.mch.weixin.qq.com/pay/orderquery";
+
+    /**
+     * 退款
+     */
+    public final  static  String REFUND=" https://api.mch.weixin.qq.com/secapi/pay/refund";
+    /**
+     * 退款查询
+     */
+    public final  static  String REFUNDQUERY=" https://api.mch.weixin.qq.com/pay/refundquery";
+
+
+
+}
