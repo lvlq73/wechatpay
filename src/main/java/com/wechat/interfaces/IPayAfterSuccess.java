a08f73550afebae7313949cdc7d962e6628a742b
lvlq73 <41459910+lvlq73@users.noreply.github.com>
2018-07-20 12:39:34 +0800

    Add files via upload

---
+++ src/main/java/com/wechat/interfaces/IPayAfterSuccess.java
@@ -0,0 +1,14 @@
+package com.wechat.interfaces;
+
+import java.util.HashMap;
+
+/**
+ * 支付成功后处理接口
+ */
+public interface IPayAfterSuccess {
+    /**
+     * 支付成功后自定义方法
+     * @param result
+     */
+    public void afterPaySuccess(HashMap<String,Object> result );
+}
