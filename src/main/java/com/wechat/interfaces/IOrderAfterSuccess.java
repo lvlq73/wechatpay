7b593265534bdca89a9ec8b8ea596fa58e46b9b2
lvlq73 <41459910+lvlq73@users.noreply.github.com>
2018-07-20 12:38:41 +0800

    Create IOrderAfterSuccess.java

---
+++ src/main/java/com/wechat/interfaces/IOrderAfterSuccess.java
@@ -0,0 +1,14 @@
+package com.wechat.interfaces;
+
+import java.util.HashMap;
+
+/**
+ * 统一下单成功后接口
+ */
+public interface IOrderAfterSuccess {
+    /**
+     * 统一下单成功后自定义处理方法
+     * @param result
+     */
+    public void afterSuccess(HashMap<String,Object> result);
+}
