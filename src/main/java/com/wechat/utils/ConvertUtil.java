a83cc00ea7b14954251962914ca353827bce792b
帽子平头 <41459910+lvlq73@users.noreply.github.com>
2019-08-05 10:47:28 +0800

    Add files via upload

--- src/main/java/com/wechat/utils/ConvertUtil.java
+++ src/main/java/com/wechat/utils/ConvertUtil.java
@@ -1,20 +1,21 @@
-package com.wechat.utils;
+package cn.com.aperfect.auap.external.pay.wechat.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.core.util.QuickWriter;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
-import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
-import com.thoughtworks.xstream.io.xml.XppDriver;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
-import java.io.Writer;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 
@@ -25,7 +26,7 @@ public class ConvertUtil {
 
     public static Logger logger = LoggerFactory.getLogger(ConvertUtil.class);
 
-    private static XmlMapper xmlMapper = new XmlMapper();
+    private static  XmlMapper xmlMapper = new XmlMapper();
     /**
      * map转xml String
      * @param obj
@@ -49,6 +50,7 @@ public class ConvertUtil {
         HashMap<String,Object> map = new HashMap<String,Object>();
         Document doc = null;
         try {
+            preventXXE(xml);
             doc = DocumentHelper.parseText(xml); // 将字符串转为XML
             Element rootElt = doc.getRootElement(); // 获取根节点
             List<Element> list = rootElt.elements();//获取根节点下所有节点
@@ -56,10 +58,72 @@ public class ConvertUtil {
                 map.put(element.getName(), element.getText()); //节点的name为map的key，text为map的value
             }
         } catch (DocumentException e) {
-            e.printStackTrace();
+            logger.error("xml转map异常:"+e.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("xml转map异常:"+e.getMessage());
         }
         return map;
     }
+
+    /**
+     * 检验是否存在XXE攻击
+     * @param xml
+     */
+    public static void preventXXE(String xml){
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        String FEATURE = null;
+        try {
+            InputStream in = new ByteArrayInputStream(xml.getBytes("UTF-8"));
+            // This is the PRIMARY defense. If DTDs (doctypes) are disallowed, almost all XML entity attacks are prevented
+            // Xerces 2 only - http://xerces.apache.org/xerces2-j/features.html#disallow-doctype-decl
+            FEATURE = "http://apache.org/xml/features/disallow-doctype-decl";
+            dbf.setFeature(FEATURE, true);
+
+            // If you can't completely disable DTDs, then at least do the following:
+            // Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-general-entities
+            // Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-general-entities
+            // JDK7+ - http://xml.org/sax/features/external-general-entities
+            FEATURE = "http://xml.org/sax/features/external-general-entities";
+            dbf.setFeature(FEATURE, false);
+
+            // Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-parameter-entities
+            // Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-parameter-entities
+            // JDK7+ - http://xml.org/sax/features/external-parameter-entities
+            FEATURE = "http://xml.org/sax/features/external-parameter-entities";
+            dbf.setFeature(FEATURE, false);
+
+            // Disable external DTDs as well
+            FEATURE = "http://apache.org/xml/features/nonvalidating/load-external-dtd";
+            dbf.setFeature(FEATURE, false);
+
+            // and these as well, per Timothy Morgan's 2014 paper: "XML Schema, DTD, and Entity Attacks"
+            dbf.setXIncludeAware(false);
+            dbf.setExpandEntityReferences(false);
+
+            // And, per Timothy Morgan: "If for some reason support for inline DOCTYPEs are a requirement, then
+            // ensure the entity settings are disabled (as shown above) and beware that SSRF attacks
+            // (http://cwe.mitre.org/data/definitions/918.html) and denial
+            // of service attacks (such as billion laughs or decompression bombs via "jar:") are a risk."
+
+            // remaining parser logic
+            DocumentBuilder safebuilder = dbf.newDocumentBuilder();
+            safebuilder.parse(in);
+        } catch (ParserConfigurationException e) {
+            // This should catch a failed setFeature feature
+            logger.info("ParserConfigurationException was thrown. The feature '" +
+                    FEATURE + "' is probably not supported by your XML processor.");
+            throw new RuntimeException("xml格式异常");
+        }
+        catch (SAXException e) {
+            // On Apache, this should be thrown when disallowing DOCTYPE
+            logger.warn("A DOCTYPE was passed into the XML document");
+            throw new RuntimeException("存在攻击XXE攻击");
+        }
+        catch (IOException e) {
+            // XXE that points to a file that doesn't exist
+            logger.error("IOException occurred, XXE may still possible: " + e.getMessage());
+            throw new RuntimeException("存在攻击XXE攻击");
+        }
+    }
 }
+
